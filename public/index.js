71f0728a14b9704be97ff29b806b867454a080f6
Didaca <D.V.Ivanov@students.softuni.bg>
2023-12-01 21:02:38 +0200

    Add some functions

--- public/index.js
+++ public/index.js
@@ -1,5 +1,81 @@
 "use strict";
 function simpleCalculator() {
+    let a;
+    let b;
+    const zero = '0';
+    const screenElement = document.getElementById('screen');
+    const clearElement = document.querySelector('.clear');
+    const plus_minusElement = document.querySelector('.plus-minus');
+    const addElement = document.querySelector('.add');
+    const subElement = document.querySelector('.sub');
+    const multiplyingElement = document.querySelector('.multiplying');
+    const divideElement = document.querySelector('.divide');
+    const resultElement = document.querySelector('.btn-e');
+    const pointElement = document.querySelector('.point');
+    const numbersElement = [...Array.from(document.querySelectorAll('.int'))];
+    const sum = (a, b) => {
+        return a + b;
+    };
+    const subtract = (a, b) => {
+        return a - b;
+    };
+    const multiplying = (a, b) => {
+        return a * b;
+    };
+    const divide = (a, b) => {
+        return a / b;
+    };
+    const math_sign = (a) => {
+        if (a.startsWith('-')) {
+            return a.slice(1);
+        }
+        return '-' + a;
+    };
+    const addNumber = (ev) => {
+        const new_value = ev.target;
+        const old_v = screenElement.textContent;
+        if (old_v) {
+            const sum = old_v + new_value.textContent;
+            if (sum.startsWith('0')) {
+                showResult(String(sum.slice(1)));
+            }
+            else {
+                showResult(String(sum));
+            }
+        }
+    };
+    const addComa = () => {
+        const value = screenElement.textContent;
+        if (!value?.includes('.')) {
+            showResult(screenElement.textContent + '.');
+        }
+    };
+    const addSymbol = (ev) => {
+        const symbolElement = ev.target;
+        const symbol = symbolElement.textContent;
+        const compare = screenElement.textContent;
+        if (symbol && !compare?.includes('+') && !compare?.includes('-') && !compare?.includes('*') && !compare?.includes('/')) {
+            screenElement.textContent += symbol;
+        }
+    };
+    const showResult = (value) => { screenElement.textContent = value; };
+    const clear = () => {
+        showResult(zero);
+    };
+    const goPlus_Minus = () => {
+        const value = String(screenElement.textContent);
+        showResult(math_sign(value));
+    };
+    clearElement.addEventListener('click', clear);
+    plus_minusElement.addEventListener('click', goPlus_Minus);
+    pointElement.addEventListener('click', addComa);
+    numbersElement.forEach(btn => {
+        btn.addEventListener('click', addNumber);
+    });
+    addElement.addEventListener('click', addSymbol);
+    subElement.addEventListener('click', addSymbol);
+    multiplyingElement.addEventListener('click', addSymbol);
+    divideElement.addEventListener('click', addSymbol);
 }
 ;
 simpleCalculator();
--- resourse/index.ts
+++ resourse/index.ts
@@ -1,5 +1,130 @@
 function simpleCalculator() {
+
+    let a: number;
+    let b: number;
+    const zero: string = '0';
+
+    const screenElement = document.getElementById('screen') as HTMLParagraphElement;
+    const clearElement = document.querySelector('.clear') as HTMLButtonElement;
+    const plus_minusElement = document.querySelector('.plus-minus') as HTMLButtonElement;
+    const addElement = document.querySelector('.add') as HTMLButtonElement;
+    const subElement = document.querySelector('.sub') as HTMLButtonElement;
+    const multiplyingElement = document.querySelector('.multiplying') as HTMLButtonElement;
+    const divideElement = document.querySelector('.divide') as HTMLButtonElement;
+    const resultElement = document.querySelector('.btn-e') as HTMLButtonElement;
+    const pointElement = document.querySelector('.point') as HTMLButtonElement;
+
+  
+    const numbersElement = [...Array.from(document.querySelectorAll('.int')!)];
     
+    /* base functions */
+    
+    /* -----------math. operations---------- */
+    const sum = (a: number, b: number):number => {
+        return a + b
+      };
+    
+    const subtract = (a: number, b: number): number => {
+    return a - b
+    };
+    
+    const multiplying = (a: number, b: number):number => {
+        return a * b
+    };
+    
+    const divide = (a: number, b: number): number => {
+        return a / b
+    };
+    
+    /* ------add/remove sign------ */
+    
+    const math_sign = (a: string): string => {
+        
+        if (a.startsWith('-')) {
+            return a.slice(1);
+        }
+        
+        return '-' + a;
+       };
+    
+    const addNumber = (ev: Event) => {
+        const new_value = ev.target as HTMLButtonElement;
+        const old_v = screenElement.textContent;
+        if (old_v) {
+
+            const sum = old_v + new_value.textContent;
+
+            if (sum.startsWith('0')) {
+
+                showResult(String(sum.slice(1)));    
+            }
+            else {
+
+                showResult(String(sum));
+            }
+        }
+    }
+
+    const addComa = () => {
+        
+        const value = screenElement.textContent;
+
+        if (!value?.includes('.')) {
+
+            showResult(screenElement.textContent + '.');
+        }
+
+    };
+
+    const addSymbol = (ev: Event) => {
+        const symbolElement = ev.target as HTMLButtonElement;
+        const symbol = symbolElement.textContent;
+        const compare = screenElement.textContent;
+
+        if (symbol && !compare?.includes('+') && !compare?.includes('-') && !compare?.includes('*') && !compare?.includes('/')) {
+
+            screenElement.textContent += symbol
+        }
+
+    };
+
+    
+    const showResult = (value: string) => { screenElement.textContent = value };
+
+   
+    /* clear screen function*/
+   const clear = ():void => {
+       showResult(zero);
+   };
+
+
+   const goPlus_Minus = (): void => {
+
+        const value = String(screenElement.textContent);
+    
+        showResult(math_sign(value));
+   }
+
+   /* add Events*/
+   clearElement.addEventListener('click', clear);
+   
+   plus_minusElement.addEventListener('click', goPlus_Minus);
+
+   pointElement.addEventListener('click', addComa);
+
+   numbersElement.forEach(btn => {
+    btn.addEventListener('click', addNumber);
+   });
+
+   addElement.addEventListener('click', addSymbol);
+   subElement.addEventListener('click', addSymbol);
+   multiplyingElement.addEventListener('click', addSymbol);
+   divideElement.addEventListener('click', addSymbol);
+
+
+   
 };
+
+
 
 simpleCalculator()
